3ce89b62a9b82c8fa08dab403b6b8caaed8ca9a5
Andre Popovitch <andre@popovit.ch>
2022-08-29 22:30:44 -0500

    switch to sphere

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,3 @@
-//! Loads and renders a glTF file as a scene.
-
-use bevy::prelude::*;
-
-pub fn app() -> App {
-    let mut app = App::new();
-    app.insert_resource(AmbientLight {
-        color: Color::WHITE,
-        brightness: 1.0 / 5.0f32,
-    })
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(setup)
-    .add_system(animate_light_direction);
-    app
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(0.7, 0.7, 1.0)
-            .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
-        ..default()
-    });
-    const HALF_SIZE: f32 = 1.0;
-    commands.spawn_bundle(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            shadow_projection: OrthographicProjection {
-                left: -HALF_SIZE,
-                right: HALF_SIZE,
-                bottom: -HALF_SIZE,
-                top: HALF_SIZE,
-                near: -10.0 * HALF_SIZE,
-                far: 10.0 * HALF_SIZE,
-                ..default()
-            },
-            shadows_enabled: true,
-            ..default()
-        },
-        ..default()
-    });
-    commands.spawn_bundle(SceneBundle {
-        scene: asset_server.load("FlightHelmet/FlightHelmet.gltf#Scene0"),
-        ..default()
-    });
-}
-
-fn animate_light_direction(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<DirectionalLight>>,
-) {
-    for mut transform in &mut query {
-        transform.rotation = Quat::from_euler(
-            EulerRot::ZYX,
-            0.0,
-            time.seconds_since_startup() as f32 * std::f32::consts::TAU / 10.0,
-            -std::f32::consts::FRAC_PI_4,
-        );
-    }
-}
-/*
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
@@ -116,8 +57,7 @@ fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
             0.0, 4.0, 0.0,
         )))
         .insert_bundle(SceneBundle {
-            scene: asset_server.load("sphere.gltf"),
+            scene: asset_server.load("sphere/sphere.gltf#Scene0"),
             ..default()
         });
 }
-*/
