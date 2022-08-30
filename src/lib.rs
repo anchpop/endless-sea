91982a2fa38f502e0c96ffbd7b3c69e5e29cf16f
Quincy <mansonqu@msu.edu>
2022-08-29 23:13:17 -0400

    bla

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,123 @@
-use bevy::prelude::*;
+//! Loads and renders a glTF file as a scene.
 
-pub const LAUNCHER_TITLE: &str = "Endless Sea";
+use bevy::prelude::*;
 
 pub fn app() -> App {
     let mut app = App::new();
-    app.insert_resource(WindowDescriptor {
-        title: LAUNCHER_TITLE.to_string(),
-        canvas: Some("#bevy".to_string()),
-        fit_canvas_to_parent: true,
-        ..Default::default()
+    app.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 1.0 / 5.0f32,
     })
     .add_plugins(DefaultPlugins)
-    .add_startup_system(load_icon);
+    .add_startup_system(setup)
+    .add_system(animate_light_direction);
     app
 }
 
-fn load_icon(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("bevy.png"),
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(0.7, 0.7, 1.0)
+            .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        ..default()
+    });
+    const HALF_SIZE: f32 = 1.0;
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..default()
+            },
+            shadows_enabled: true,
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn_bundle(SceneBundle {
+        scene: asset_server.load("FlightHelmet/FlightHelmet.gltf#Scene0"),
         ..default()
     });
 }
+
+fn animate_light_direction(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<DirectionalLight>>,
+) {
+    for mut transform in &mut query {
+        transform.rotation = Quat::from_euler(
+            EulerRot::ZYX,
+            0.0,
+            time.seconds_since_startup() as f32 * std::f32::consts::TAU / 10.0,
+            -std::f32::consts::FRAC_PI_4,
+        );
+    }
+}
+/*
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+pub fn app() -> App {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_startup_system(setup_graphics)
+        .add_startup_system(setup_physics);
+    app
+}
+
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-3.0, 3.0, 10.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    const HALF_SIZE: f32 = 1.0;
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..default()
+            },
+            shadows_enabled: true,
+            ..default()
+        },
+        ..default()
+    });
+}
+
+fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
+    /* Create the ground. */
+    commands
+        .spawn()
+        .insert(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(
+            0.0, -2.0, 0.0,
+        )));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn()
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(
+            0.0, 4.0, 0.0,
+        )))
+        .insert_bundle(SceneBundle {
+            scene: asset_server.load("sphere.gltf"),
+            ..default()
+        });
+}
+*/
