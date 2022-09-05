b149b6ba09eaa998b4f5bb83cb2992554cc6b586
Andre Popovitch <andre@popovit.ch>
2022-09-04 19:33:16 -0500

    add character module (with plugin and bundle)

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,10 @@
 #![allow(clippy::type_complexity)]
+mod character;
+use character::*;
+
 use bevy::{prelude::*, render::camera::ScalingMode, time::Stopwatch};
 use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin};
 use bevy_rapier3d::prelude::*;
-
-#[derive(Inspectable, Reflect, Component, Default, Clone)]
-#[reflect(Component)]
-struct PlayerCharacter;
-
-#[derive(Inspectable, Reflect, Component, Default, Clone)]
-#[reflect(Component)]
-struct CharacterMovementProperties {
-    stopped_friction: f32,
-    acceleration: f32,
-    damping_factor: f32,
-    max_speed: f32,
-    jump_impulse: f32,
-}
-
-#[derive(Reflect, Component, Default, Clone)]
-#[reflect(Component)]
-enum JumpState {
-    #[default]
-    Normal,
-    Charging(Stopwatch),
-    JumpPressed(Stopwatch),
-}
-
-#[derive(Reflect, Component, Default, Clone)]
-#[reflect(Component)]
-struct CharacterInput {
-    direction: Vec3,
-    jump: JumpState,
-}
 
 #[derive(Inspectable, Reflect, Component, Default, Clone)]
 #[reflect(Component)]
@@ -51,11 +24,10 @@ pub fn app() -> App {
     })
     .add_plugins(DefaultPlugins)
     .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+    .add_plugin(CharacterPlugin)
     .add_startup_system(setup_graphics)
     .add_startup_system(setup_physics)
     .add_system(player_input)
-    .add_system(force_movement.after(player_input))
-    .add_system(impluse_movement.after(player_input))
     .add_stage_after(
         PhysicsStages::Writeback,
         POST_SIMULATION,
@@ -124,37 +96,12 @@ fn setup_physics(
     /* Create the bouncing ball. */
     commands
         .spawn()
-        .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(0.5))
-        .insert(Restitution::coefficient(0.0))
-        .insert(LockedAxes::ROTATION_LOCKED)
-        .insert(Velocity::default())
-        .insert_bundle(SpatialBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)))
         .insert_bundle(SceneBundle {
             scene: asset_server.load("sphere/sphere.gltf#Scene0"),
             ..default()
         })
+        .insert_bundle(CharacterBundle::default())
         .insert(PlayerCharacter {})
-        .insert(CharacterMovementProperties {
-            stopped_friction: 4.0,
-            acceleration: 10.0,
-            damping_factor: 30.0,
-            max_speed: 10.0,
-            jump_impulse: 3.0,
-        })
-        .insert(CharacterInput::default())
-        .insert(ExternalForce {
-            force: Vec3::new(0., 0., 0.),
-            torque: Vec3::new(0., 0., 0.),
-        })
-        .insert(ExternalImpulse {
-            impulse: Vec3::new(0., 0., 0.),
-            torque_impulse: Vec3::new(0., 0., 0.),
-        })
-        .insert(Friction {
-            coefficient: 0.0,
-            combine_rule: CoefficientCombineRule::Max,
-        })
         .insert(Name::new("Ball"));
 
     /* Create an obstacle. */
@@ -245,104 +192,6 @@ fn player_input(
                 JumpState::JumpPressed(_watch) => {
                     character_input.jump = JumpState::Normal;
                 }
-            }
-        }
-    }
-}
-
-fn force_movement(
-    mut player_character: Query<(
-        &CharacterInput,
-        &CharacterMovementProperties,
-        &Velocity,
-        &mut ExternalForce,
-        &mut Friction,
-    )>,
-) {
-    fn project_onto_plane(v: Vec3, n: Vec3) -> Vec3 {
-        v - v.project_onto(n)
-    }
-    if let Some((
-        character_input,
-        character_movement_properties,
-        velocity,
-        mut external_force,
-        mut friction,
-    )) = player_character.iter_mut().next()
-    {
-        let velocity_direction_difference = velocity
-            .linvel
-            .try_normalize()
-            .map(|v| {
-                project_onto_plane(character_input.direction, Vec3::Y)
-                    - project_onto_plane(v, Vec3::Y)
-            })
-            .unwrap_or(Vec3::ZERO);
-
-        if character_input.direction != Vec3::ZERO {
-            let under_max_speed = velocity
-                .linvel
-                .project_onto(character_input.direction)
-                .length()
-                < character_movement_properties.max_speed;
-            let directional_force = if under_max_speed {
-                character_input.direction
-                    * character_movement_properties.acceleration
-            } else {
-                Vec3::ZERO
-            };
-            let damping_force = velocity_direction_difference
-                * character_movement_properties.damping_factor;
-            external_force.force = directional_force + damping_force;
-            friction.coefficient = 0.0;
-        } else {
-            friction.coefficient =
-                character_movement_properties.stopped_friction;
-            external_force.force = character_input.direction;
-        }
-    } else {
-        println!("No player character found!");
-    }
-}
-
-fn impluse_movement(
-    rapier_context: Res<RapierContext>,
-    mut player_character: Query<(
-        Entity,
-        &CharacterInput,
-        &CharacterMovementProperties,
-        &Transform,
-        &mut ExternalImpulse,
-    )>,
-) {
-    if let Some((
-        entity,
-        character_input,
-        character_movement_properties,
-        transform,
-        mut external_impulse,
-    )) = player_character.iter_mut().next()
-    {
-        if let Some((_entity, _toi)) = rapier_context.cast_ray(
-            // TODO: Should use a shapecast instead
-            transform.translation,
-            Vec3::NEG_Y,
-            1.1,
-            true,
-            QueryFilter {
-                exclude_collider: Some(entity),
-                ..default()
-            },
-        ) {
-            if let JumpState::JumpPressed(_watch) = character_input.jump.clone()
-            {
-                external_impulse.impulse = Vec3::new(
-                    0.,
-                    character_movement_properties.jump_impulse,
-                    0.,
-                );
-            } else {
-                external_impulse.impulse = Vec3::ZERO;
             }
         }
     }
