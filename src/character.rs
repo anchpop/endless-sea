d620f00256c084ba0f7522ef9a3ff8a9c8e6f7e6
Quincy <mansonqu@msu.edu>
2022-09-07 23:04:10 -0400

    clippy

--- src/character.rs
+++ src/character.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use bevy::{prelude::*, time::Stopwatch};
-use bevy_inspector_egui::{Inspectable, RegisterInspectable};
+use bevy_inspector_egui::Inspectable;
 use bevy_rapier3d::prelude::*;
 
 // Plugin
@@ -17,10 +17,7 @@ impl Plugin for CharacterPlugin {
                 update_grounded
                     .before(impulse_movement)
                     .before(force_movement),
-            )/* 
-            .register_inspectable::<PlayerCharacter>()
-            .register_inspectable::<Character>()
-            .register_inspectable::<CharacterMovementProperties>()*/;
+            );
     }
 }
 
