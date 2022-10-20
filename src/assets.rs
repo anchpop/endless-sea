85719de2bb5ff4d220a4bd70354e1876b6152bd9
Andre Popovitch <andre@popovit.ch>
2022-10-20 12:14:25 -0500

    switch gltf for glb

--- src/assets.rs
+++ src/assets.rs
@@ -5,12 +5,12 @@ use bevy_asset_loader::prelude::*;
 pub struct Assets {
     #[asset(path = "floor/floor.gltf#Scene0")]
     pub floor: Handle<Scene>,
-    #[asset(path = "capsule/capsule.gltf#Scene0")]
+    #[asset(path = "capsule/capsule.glb#Scene0")]
     pub character: Handle<Scene>,
     #[asset(path = "cube/cube.gltf#Scene4")]
     pub cube: Handle<Scene>,
-    #[asset(path = "sword/sword.gltf#Scene0")]
+    #[asset(path = "sword/sword.glb#Scene0")]
     pub sword: Handle<Scene>,
-    #[asset(path = "gun/gun.gltf#Scene0")]
+    #[asset(path = "gun/gun.glb#Scene0")]
     pub gun: Handle<Scene>,
 }
