343fc6eb80b9a168f4b1ebb3b688a4b3ee93a759
Andre Popovitch <andre@popovit.ch>
2023-04-09 03:13:57 -0400

    Update Yew and Stylist

--- launchers/wasm/src/main.rs
+++ launchers/wasm/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+//use bevy::prelude::*;
 use endless_sea::LAUNCHER_TITLE;
 use stylist::{css, global_style, yew::styled_component};
 use yew::{events::MouseEvent, prelude::*};
@@ -28,32 +28,45 @@ fn set_global_css() {
     .expect("Unable to mount global style");
 }
 
-#[styled_component(Root)]
-fn view() -> Html {
-    set_window_title(LAUNCHER_TITLE);
-    set_global_css();
+pub struct App;
 
-    let css = css!(
-        r#"
-        position: absolute;
-        overflow: hidden;
-        width: 100%;
-        height: 100%;
-        "#
-    );
+impl Component for App {
+    type Message = ();
+    type Properties = ();
 
-    html! {
-        <div class={ css }>
-            <canvas id="bevy" oncontextmenu={|mouse_event: MouseEvent| mouse_event.prevent_default() }></canvas>
-        </div>
+    fn create(_ctx: &Context<Self>) -> Self {
+        set_window_title(LAUNCHER_TITLE);
+        set_global_css();
+
+        Self
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let styles = css!(
+            r#"
+            position: absolute;
+            overflow: hidden;
+            width: 100%;
+            height: 100%;
+            "#
+        );
+
+        html! {
+            <div class={styles}>
+                <canvas id="bevy" oncontextmenu={|mouse_event: MouseEvent| mouse_event.prevent_default() }></canvas>
+                <p>{"test"}</p>
+            </div>
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            let mut app = endless_sea::app();
+            app.run();
+        }
     }
 }
 
 fn main() {
-    // Mount the DOM
-    yew::start_app::<Root>();
-    // Start the Bevy App
-    let mut app = endless_sea::app();
-    info!("Starting launcher: WASM");
-    app.run();
+    yew::Renderer::<App>::new().render();
 }
